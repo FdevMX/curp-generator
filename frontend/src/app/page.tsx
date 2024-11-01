6a718405f38fa400907a5f822d9d5d3e76d7f0ff
fdevmx <filesforcloud.fdev.mx@gmail.com>
2024-11-01 05:29:22 -0600

    pull full

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,5 @@
+import { CurpGenerator } from "@/components/curp-generator"
+
+export default function Page() {
+  return <CurpGenerator />
+}
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { CurpGenerator } from "@/src/components/curp-generator"
-
-export default function Page() {
-  return <CurpGenerator />
-}
