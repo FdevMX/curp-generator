8806e90b1bcf3729510750f88d9e5d27fa964c82
fdevmx <filesforcloud.fdev.mx@gmail.com>
2024-11-01 05:42:16 -0600

    frontend up2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/analyze',
+        destination: 'https://curp-generator-backend.vercel.app/analyze', // Ajusta esta URL
+        // destination: 'http://localhost:5000/analyze', // Asume que Flask está corriendo en el puerto 5000
+        // destination: '/analyze', // Esto sigue apuntando a la ruta del backend
+      },
+    ]
+  },
+  // Asegúrate de que el output sea 'standalone' para Vercel
+  // output: 'standalone',
 };
 
 export default nextConfig;
